b667983305dd04d4b7029829fcce59097d067112
jubayer <mohammadjubayer999@gmail.com>
2019-10-28 12:31:27 +0000

    test

--- shop/shop_registration.php
+++ shop/shop_registration.php
@@ -24,5 +24,5 @@ if($staff->check_role() == 'Admin'){
 	}
 }else{
 	
-	echo json_encode(['status' => false , 'message' => "You do not have the permission to perform this action!"]);exit;
+	echo json_encode(['status' => false , 'message' => "You do not have the permission to perform this actions!"]);exit;
 }
